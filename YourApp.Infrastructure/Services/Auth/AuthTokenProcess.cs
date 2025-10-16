8e3a9d257083827164425a2a5f5c56828c61b742
kleqing <nhanpk28122004@gmail.com>
2025-10-16 20:49:07 +0700

    [Update] Fix userId always get sub as default value cause email confirmation didn't find userId

--- YourApp.Infrastructure/Services/Auth/AuthTokenProcess.cs
+++ YourApp.Infrastructure/Services/Auth/AuthTokenProcess.cs
@@ -123,7 +123,8 @@ public class AuthTokenProcess : IAuthTokenProcess
 
             var principal = tokenHandler.ValidateToken(token, parameters, out _);
             var purpose = principal.FindFirst("purpose")?.Value;
-            var userId = principal.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+            var userId = principal.FindFirst(JwtRegisteredClaimNames.Sub)?.Value ??
+                         principal.FindFirst(ClaimTypes.NameIdentifier)?.Value; //* Fallback to NameIdentifier if Sub is not found
 
             return purpose == "email_confirmation" && userId == user.UserId.ToString();
         }
@@ -132,7 +133,6 @@ public class AuthTokenProcess : IAuthTokenProcess
             return false;
         }
     }
-
     
     private CookieOptions BuildCookieOptions(DateTime? expiry = null)
     {
